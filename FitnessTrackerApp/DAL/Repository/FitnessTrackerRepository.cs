1478ba2457ddf87f4120b7c8835c94e2a4704c6e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-04 15:06:01 +0530

    Add files via upload

---
+++ FitnessTrackerApp/DAL/Repository/FitnessTrackerRepository.cs
@@ -0,0 +1,64 @@
+﻿using FitnessTrackerApp.DAL.Interface;
+using FitnessTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FitnessTrackerApp.DAL.Repository
+{
+    public class FitnessTrackerRepository : IFitnessTrackerRepository
+    {
+        private FitnessTrackerDbContext _context;
+        public FitnessTrackerRepository(FitnessTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public async Task<IEnumerable<Workout>> GetWorkouts()
+        {
+             return _context.Workouts.ToList();
+        }
+        public async Task<Workout> GetWorkoutByID(int id)
+        {
+            return _context.Workouts.Find(id);
+        }
+        public async Task<Workout> InsertWorkout(Workout workout)
+        {
+            return _context.Workouts.Add(workout);
+        }
+        public async Task<int> DeleteWorkout(int workoutID)
+        {
+            Workout workout = _context.Workouts.Find(workoutID);
+            var res= _context.Workouts.Remove(workout);
+            return res.Id;
+        }
+        public async Task<bool> UpdateWorkout(Workout workout)
+        {
+            var res= _context.Entry(workout).State = EntityState.Modified;
+            return res.Equals("workout");
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ FitnessTrackerApp/DAL/Repository/FitnessTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using FitnessTrackerApp.DAL.Interface;
+using FitnessTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FitnessTrackerApp.DAL.Repository
+{
+    public class FitnessTrackerService : IFitnessTrackerInterface
+    {
+        private IFitnessTrackerRepository _repo;
+        public FitnessTrackerService(IFitnessTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public async Task<int> DeleteWorkout(int workoutId)
+        {
+            var res= _repo.DeleteWorkout(workoutId);
+            return res.Id;
+        }
+
+        public Task<Workout> GetWorkoutByID(int workoutId)
+        {
+            return _repo.GetWorkoutByID(workoutId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        Task<IEnumerable<Workout>> IFitnessTrackerInterface.GetWorkouts()
+        {
+            return (Task<IEnumerable<Workout>>)(IEnumerable<Workout>)_repo.GetWorkouts();
+        }
+
+        Task<Workout> IFitnessTrackerInterface.InsertWorkout(Workout workout)
+        {
+            return _repo.InsertWorkout(workout);
+        }
+
+        Task<bool> IFitnessTrackerInterface.UpdateWorkout(Workout workout)
+        {
+            return _repo.UpdateWorkout(workout);
+        }
+    }
+}
