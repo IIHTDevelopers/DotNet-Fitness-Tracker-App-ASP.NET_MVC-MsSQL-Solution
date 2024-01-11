7819df998c21c4cf5c5763a8e1237bc787e59934
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 10:38:48 +0530

    Add files via upload

---
+++ FitnessTrackerApp/DAL/Interface/IFitnessTrackerInterface.cs
@@ -0,0 +1,19 @@
+﻿using FitnessTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTrackerApp.DAL.Interface
+{
+    public interface IFitnessTrackerInterface 
+    {
+        IEnumerable<Workout> GetWorkouts();
+        Workout GetWorkoutByID(int workoutId);
+        Workout InsertWorkout(Workout workout);
+        int DeleteWorkout(int workoutId);
+        bool UpdateWorkout(Workout workout);
+        void Save();
+    }
+}
---
+++ FitnessTrackerApp/DAL/Interface/IFitnessTrackerRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using FitnessTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTrackerApp.DAL.Interface
+{
+    public interface IFitnessTrackerRepository
+    {
+        IEnumerable<Workout> GetWorkouts();
+        Workout GetWorkoutByID(int workoutId);
+        Workout InsertWorkout(Workout workout);
+        int DeleteWorkout(int workoutId);
+        bool UpdateWorkout(Workout workout);
+        void Save();
+    }
+}
